d7b50a8cce94e54d2b48fcc37201236fd3237aeb
abujayed007 <abujayed007@gmail.com>
2022-10-27 22:48:39 +0600

    node module commit

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+
 const sports = require('./data/sports.json')
+
+
+
+app.get('/', (req, res) =>{
+    res.send('Node is running')
+})
 
 app.get('/sports', (req, res) =>{
     res.send(sports)
 })
-
-app.get('/', (req, res) =>{
-    res.send('Node is running')
+app.get('/sports/:name', (req, res) =>{
+    const name = req.params.name;
+    const selectSports = sports.find(sport => sport.name === name)
+    res.send(selectSports)
 })
 
 app.listen(port, () =>{
